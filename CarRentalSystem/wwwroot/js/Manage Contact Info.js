01c87468d02a0a7fe6fec57d9b3dd22656925f35
Danuharan <r.danuharan@gmail.com>
2025-09-21 03:11:32 +0530

    Front end 95% comleted udate (Danuharan on 21st 3.11am)

---
+++ CarRentalSystem/wwwroot/js/Manage Contact Info.js
@@ -0,0 +1,17 @@
+﻿  /* this Js code for hide the side bar symbal in Admin side - sidebar panel. After click the symbal side bar open and the symbal will be hidden.  */
+    document.addEventListener('DOMContentLoaded', function () {
+            const adminSidebar = document.getElementById('adminSidebar');
+    const sidebarToggle = document.querySelector('.sidebar-toggle');
+
+    if (adminSidebar && sidebarToggle) {
+        // When the sidebar starts to open, add the 'is-hidden' class to the button
+        adminSidebar.addEventListener('show.bs.offcanvas', () => {
+            sidebarToggle.classList.add('is-hidden');
+        });
+
+                // When the sidebar is fully closed, remove the 'is-hidden' class
+                adminSidebar.addEventListener('hidden.bs.offcanvas', () => {
+        sidebarToggle.classList.remove('is-hidden');
+                });
+            }
+        });
